dfba67c7af9c7097897ce16b753ef549aa74abf3
Jack Seemer <admin@AWODs-MBP.fios-router.home>
2018-08-30 18:37:43 -0400

    Add analytics

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -14,6 +14,18 @@ const TemplateWrapper = ({ children }) => (
       <link href="https://fonts.googleapis.com/css?family=Josefin+Sans|Roboto" rel="stylesheet"/>
       <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
       <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-125006873-1"></script>
+      <script>
+        {
+          `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-125006873-1');
+          `
+        }
+      </script>
     </Helmet>
     <Navbar />
     <MainContent>{children()}</MainContent>
