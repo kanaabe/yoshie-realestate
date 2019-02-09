6d97a7635f25ac63ef47c959f5f91949f6200989
Kanako Abe <kanakoabe5@gmail.com>
2019-02-09 12:02:04 -0500

    Lighter overlay color

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,10 +1,10 @@
-import React from "react";
-import PropTypes from "prop-types";
-import styled from "styled-components";
-import { Paragraph } from "./Common";
-import { HTMLContent } from "./Content";
-import remark from "remark";
-import remarkHtml from "remark-html";
+import React from "react"
+import PropTypes from "prop-types"
+import styled from "styled-components"
+import { Paragraph } from "./Common"
+import { HTMLContent } from "./Content"
+import remark from "remark"
+import remarkHtml from "remark-html"
 
 const FeatureGrid = ({ gridItems }) => (
   <div className="columns is-multiline">
@@ -12,7 +12,7 @@ const FeatureGrid = ({ gridItems }) => (
       const textHTML = remark()
         .use(remarkHtml)
         .processSync(item.text)
-        .toString();
+        .toString()
 
       return (
         <div key={item.image} className="column is-6">
@@ -30,32 +30,31 @@ const FeatureGrid = ({ gridItems }) => (
             <HTMLContent content={textHTML} />
           </Paragraph>
         </div>
-      );
+      )
     })}
   </div>
-);
+)
 
 const StatusOverlay = styled.div`
   width: 100%;
-  height: 300px;
+  height: 100px;
   display: flex;
-  font-size: 60px;
-  background-color: black;
-  opacity: 0.5;
+  font-size: 50px;
+  background: rgba(0, 0, 0, 0.3);
   position: absolute;
-  top: 0;
+  top: 100px;
   justify-content: center;
   align-items: center;
   color: white;
   text-align: center;
-`;
+`
 
 const Image = styled.div`
   background-image: url("${props => props.src}");
   background-size: cover;
   height: 100%;
   background-position: 50% 50%;
-`;
+`
 
 const ImageWrapper = styled.div`
   width: 100%;
@@ -67,7 +66,7 @@ const ImageWrapper = styled.div`
     opacity: 0.5;
     background-color: black;
   }`};
-`;
+`
 
 FeatureGrid.propTypes = {
   gridItems: PropTypes.arrayOf(
@@ -78,6 +77,6 @@ FeatureGrid.propTypes = {
       status: PropTypes.string
     })
   )
-};
+}
 
-export default FeatureGrid;
+export default FeatureGrid
